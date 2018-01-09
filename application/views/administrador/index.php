febef0179ce7fb8b3cd7666419c73012689b089d
luis sanchez <luis_sanchez_arenas@hotmail.com>
2018-01-09 11:47:22 -0600

    modificacion target _self

--- application/views/administrador/index.php
+++ application/views/administrador/index.php
@@ -19,7 +19,7 @@ body{
   </div><!-- row -->
   <div class="row">
   	<div class="col-xs-6 col-sm-6 col-md-1 col-lg-1 pull-right">
-        <?php echo form_open("Coordinador/index", array("id"=>"form_administrador_mostrar", "target"=>"_blank")); ?>
+        <?php echo form_open("Coordinador/index", array("id"=>"form_administrador_mostrar", "target"=>"_self")); ?>
           <input id="itxt_administrador_id_coordinador" name="itxt_administrador_id_coordinador" type="hidden">
           <button id="btn_administrador_mostrar" type="submit" class="btn btn-primary btn-block">
             <center>
